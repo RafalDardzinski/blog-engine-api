043fe0ae9e543924ff5c877a45dc3d28d2fa5fc1
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-09-23 22:31:09 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+        "mocha": true,
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-underscore-dangle": 0,
+        "class-methods-use-this": 0,
+    }
+};
