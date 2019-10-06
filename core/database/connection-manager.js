e1e64ec5f0d5ef7b80a50b9cec5d8d2e410a6093
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-10-06 22:18:53 +0200

    added: DatabaseConnectionManager

---
+++ core/database/connection-manager.js
@@ -0,0 +1,74 @@
+const Model = require('./model');
+const { InvalidOperationError } = require('../error');
+
+const _config = new WeakMap();
+const _connection = new WeakMap();
+
+const defaultOptions = {
+  useNewUrlParser: true,
+};
+
+/**
+ * Manages connection with a database.
+ */
+class DatabaseConnectionManager {
+  /**
+   * @param {Function} connectionFactoryMethod Connection factory method.
+   * @param {DatabaseConfiguration} config Instance of dabase configuration.
+   * @param {Object} connectionObserver Contains hooks to changes in connection.
+   */
+  constructor(connectionFactoryMethod, config, connectionObserver) {
+    _config.set(this, config);
+
+    const connection = connectionFactoryMethod();
+    if (connectionObserver) {
+      connectionObserver.watch(connection);
+    }
+    _connection.set(this, connection);
+  }
+
+  /**
+   * Informs if connection has been established.
+   */
+  get isConnected() {
+    return _connection.get(this).readyState === 1;
+  }
+
+  /**
+   * Connects to the database.
+   * @param {Object=} options Connection options.
+   */
+  connect(options = {}) {
+    const connection = _connection.get(this);
+    const config = _config.get(this);
+    const opts = Object.assign({}, defaultOptions, options);
+    return connection.openUri(config.getUri(), opts);
+  }
+
+  /**
+   * Closes the connection to the database.
+   */
+  disconnect() {
+    _connection.get(this).close();
+  }
+
+  /**
+   * Registers model on connection.
+   * @param {Model} model
+   */
+  registerModel(model) {
+    if (!(model instanceof Model)) {
+      throw new InvalidOperationError('ConnectionManager can register only instances of Model class.');
+    }
+
+    const connection = _connection.get(this);
+    const { name, schema } = model;
+    return connection.model(name, schema);
+  }
+}
+
+module.exports = DatabaseConnectionManager;
+/**
+ * @typedef {import('../configuration/database')} DatabaseConfiguration
+ * @typedef {import('./model')} Model
+ */
---
+++ core/database/connection-manager.spec.js
@@ -0,0 +1,158 @@
+// Global imports
+const chai = require('chai');
+const spies = require('chai-spies');
+
+// Local imports
+const DatabaseConnectionManager = require('./connection-manager');
+
+// Mocks
+class ConnectionMock {
+  constructor() {
+    this.readyState = 0;
+  }
+
+  openUri() {
+    return Promise.resolve();
+  }
+}
+
+class DatabaseConfigMock {
+  static getUri() {
+    return 'testuri';
+  }
+}
+
+class ConnectionObserverMock {
+  watch() {
+  }
+}
+
+// Test suite setup
+chai.use(spies);
+const sandbox = chai.spy.sandbox();
+const { expect } = chai;
+
+describe(`DatabaseConnectionManager ${__dirname}`, () => {
+  let connection;
+  let connectionFactoryMethod;
+  let connectionObserver;
+  let unitUnderTest;
+
+  beforeEach(() => {
+    connection = new ConnectionMock();
+    connectionFactoryMethod = () => connection;
+    connectionObserver = new ConnectionObserverMock();
+    unitUnderTest = new DatabaseConnectionManager(
+      connectionFactoryMethod,
+      DatabaseConfigMock,
+      connectionObserver,
+    );
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('constructor(connectionFactoryMethod, config, connectionObserver)', () => {
+    it('does not reveal connectionFactoryMethod', () => {
+      // Arrange
+      const publicProps = Object.values(unitUnderTest);
+
+      // Act + Assert
+      expect(publicProps).to.not.contain(connectionFactoryMethod);
+    });
+
+    it('does not reveal config', () => {
+      // Arrange
+      const publicProps = Object.values(unitUnderTest);
+
+      // Act + Assert
+      expect(publicProps).to.not.contain(DatabaseConfigMock);
+    });
+
+    it('does not reveal connectionObserver', () => {
+      // Arrange
+      const publicProps = Object.values(unitUnderTest);
+
+      // Act + Assert
+      expect(publicProps).to.not.contain(connectionObserver);
+    });
+
+    describe('when connectionObserver is truthy', () => {
+      it('calls connectionObserver.watch() on result of connectionFactory()', () => {
+        // Arrange
+        sandbox.on(connectionObserver, 'watch');
+
+        // Act
+        unitUnderTest = new DatabaseConnectionManager(connectionFactoryMethod,
+          null,
+          connectionObserver);
+
+        // Assert
+        expect(connectionObserver.watch).to.have.been.called();
+      });
+    });
+  });
+
+  describe('DatabaseConnectionManager#isConnected', () => {
+    describe('when is connected to the database...', () => {
+      it('returns true', () => {
+        // Arrange
+        connection.readyState = 1;
+
+        // Act
+        const result = unitUnderTest.isConnected;
+
+        // Assert
+        expect(result).to.equal(true);
+      });
+    });
+
+    describe('when is not connected to the database...', () => {
+      it('returns false', () => {
+        // Act
+        const result = unitUnderTest.isConnected;
+
+        // Arrange
+        expect(result).to.equal(false);
+      });
+    });
+  });
+
+  describe('DatabaseConnectionManager#connect(options)', () => {
+    it('connects to the database using provided options and configuration', async () => {
+      // Arrange
+      const options = {
+        useNewUrlParser: false,
+        someProp: true,
+      };
+      const uri = DatabaseConfigMock.getUri();
+      sandbox.on(connection, 'openUri');
+
+      // Act
+      await unitUnderTest.connect(options);
+
+      // Assert
+      expect(connection.openUri).to.have.been.called
+        .with(uri, options);
+    });
+
+    describe('when options are not provided', () => {
+      it('connects to the database using default options', async () => {
+        // Arrange
+        const defaultOptions = {
+          useNewUrlParser: true,
+        };
+        const uri = DatabaseConfigMock.getUri();
+        sandbox.on(connection, 'openUri');
+
+        // Act
+        await unitUnderTest.connect();
+
+        // Assert
+        expect(connection.openUri).to.have.been.called
+          .with(uri, defaultOptions);
+      });
+    });
+  });
+});
