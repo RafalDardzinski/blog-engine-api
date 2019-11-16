181bb037878c0b78aa1d1f6ba38d215d202dffa4
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-11-16 18:43:50 +0100

    added: index of core modules

---
+++ core/index.js
@@ -0,0 +1,17 @@
+const Application = require('./application');
+const Database = require('./database');
+const Error = require('./error');
+const Generics = require('./generics');
+const Logger = require('./logger');
+const Server = require('./server');
+const Web = require('./web');
+
+module.exports = {
+  Application,
+  Database,
+  Error,
+  Generics,
+  Logger,
+  Server,
+  Web,
+}
