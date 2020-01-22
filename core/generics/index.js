bb5a8632280416843a03266aedd88f4f1e8974fd
Rafał Dardziński <rafal.dardzinski@gmail.com>
2020-01-22 01:08:35 +0100

    Added: QueryFilter abstract class.

--- core/generics/index.js
+++ core/generics/index.js
@@ -2,10 +2,12 @@ const HTTP_METHODS = require('./http-methods');
 const ValidatedEntity = require('./validated-entity');
 const DataTransferObject = require('./data-transfer-object');
 const ENVIRONMENT_TYPES = require('./environment-types');
+const QueryFilter = require('./query-filter');
 
 module.exports = {
   HTTP_METHODS,
   ENVIRONMENT_TYPES,
   ValidatedEntity,
   DataTransferObject,
+  QueryFilter,
 };
