afebccac226fa164a6754994b7fdda874a6fb04e
Rafał Dardziński <rafal.dardzinski@gmail.com>
2020-02-16 22:28:16 +0100

    Adjusted: Core root is now simplified and exports fewer modules.

--- core/index.js
+++ core/index.js
@@ -6,11 +6,16 @@ const CoreModules = require('./modules');
 const CoreServer = require('./server');
 const CoreWeb = require('./web');
 
-
 // Application
-const { ApplicationFactory, ApplicationModule, ApplicationModulesManager } = CoreApplication;
+const {
+  ApplicationFactory,
+  ApplicationModule,
+  ApplicationModulesManager,
+  ApplicationInitializer,
+} = CoreApplication;
 const { permissionsManager } = CoreAuthorization;
 const webApplicationBuilder = CoreWeb.ExpressWebApplicationBuilderFactory.create();
+
 const applicationModulesManager = new ApplicationModulesManager();
 const applicationFactory = new ApplicationFactory(
   CoreModules,
@@ -19,42 +24,22 @@ const applicationFactory = new ApplicationFactory(
   applicationModulesManager,
 );
 
+const { DatabaseConnectionManagerFactory } = CoreDatabase;
+const { HttpServerFactory } = CoreServer;
+
+const applicationInitializer = new ApplicationInitializer(
+  DatabaseConnectionManagerFactory,
+  applicationFactory,
+  HttpServerFactory,
+);
+
 module.exports.Application = {
   applicationFactory,
+  applicationInitializer,
   ApplicationModule,
 };
 
-// Database
-const { DatabaseConnectionManagerFactory } = CoreDatabase;
-module.exports.Database = {
-  DatabaseConnectionManagerFactory,
-};
 
 // Error
 const { BusinessLogic } = CoreError;
 module.exports.Error = BusinessLogic;
-
-// Server
-const { HttpServerFactory } = CoreServer;
-module.exports.Server = {
-  HttpServerFactory,
-};
-
-// const Database = require('./database');
-// const { BusinessLogic } = require('./error');
-// const Generics = require('./generics');
-// const Logger = require('./logger');
-// const Server = require('./server');
-// const Web = require('./web');
-
-// module.exports = {
-//   Application: {
-//     ApplicationModule,
-//   },
-//   Database,
-//   Error: BusinessLogic,
-//   Generics,
-//   Logger,
-//   Server,
-//   Web,
-// };
