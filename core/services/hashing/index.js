c00dc9b7b7079d0a2e1427d0956400b3ebe52c92
Rafał Dardziński <rafal.dardzinski@gmail.com>
2020-03-15 19:25:41 +0100

    Refactored: Removed duplicate config initialization.

--- core/services/hashing/index.js
+++ core/services/hashing/index.js
@@ -5,19 +5,20 @@ const HashingService = require('./hashing-service');
 const Pbkdf2HashingService = require('./pbkdf2-hashing-service');
 const HashSchemaFactoryBase = require('./hash-schema');
 
+const config = new HashingServiceConfiguration();
+
+
 class HashingServiceFactory {
   /**
    * Creates a new instance of Hashing service.
    */
   static create() {
-    const config = new HashingServiceConfiguration();
     return new Pbkdf2HashingService(config);
   }
 }
 
 class HashSchemaFactory {
   static create() {
-    const config = new HashingServiceConfiguration();
     const schemaOptions = { _id: false };
     return HashSchemaFactoryBase.create(config, schemaOptions);
   }
